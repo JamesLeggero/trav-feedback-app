21bfbcebb5d2c6edafa610c2d8f79c36eef04e71
JamesLeggero <james.m.leggero@gmail.com>
2020-11-06 21:46:04 -0500

    feedback madel

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
+load_dotenv()
 import os
 
 app = Flask(__name__)
@@ -13,7 +14,25 @@ if ENV == 'dev':
 
 else:
     app.debug = False
-    
+    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Feedback(db.Model):
+    __tablename__ = 'feedback'
+    id = db.Column(db.Integer, primary_key=True)
+    customer = db.Column(db.String(200), unique=True)
+    dealer = db.Column(db.String(200))
+    rating = db.Column(db.Integer)
+    comments = db.Column(db.Text())
+
+    def __init__(self, customer, dealer, rating, comments):
+        self.customer = customer
+        self.dealer = dealer
+        self.rating = rating
+        self.comments = comments
 
 @app.route('/')
 def index():
