bd9c191aa94215ac80be6b8c940945b9d916a6e3
JamesLeggero <james.m.leggero@gmail.com>
2020-11-08 19:41:43 -0500

    sendmail working and writing to db

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 load_dotenv()
 import os
+from send_mail import send_mail
 
 app = Flask(__name__)
 
@@ -52,6 +53,7 @@ def submit():
             data = Feedback(customer, dealer, rating, comments)
             db.session.add(data)
             db.session.commit()
+            send_mail(customer, dealer, rating, comments )
             return render_template('success.html')
         return render_template('index.html', message='You have already submitted feedback')
 
